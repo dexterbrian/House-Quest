6feca56062acf5bd69e70786137a1966eda86533
Brian Waweru <91993455+dexterbrian@users.noreply.github.com>
2023-07-31 17:42:29 +0000

    Enabled opening /houses when Find a house button is clicked

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 function Search() {
-    return(
-        <>
-            <input type="text" placeholder="Type in a location..." />
-            <button>Find a house</button>
-        </>
-    );
+  const [query, setQuery] = useState("");
+  const history = useHistory();
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Type a location or leave blank..."
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault;
+          history.push("/houses", query);
+        }}
+      >
+        Find a house
+      </button>
+    </>
+  );
 }
 
 export default Search;
