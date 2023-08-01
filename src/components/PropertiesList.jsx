223fd13aa8078d443d2b97dadf6943d0829400b2
Brian Waweru <91993455+dexterbrian@users.noreply.github.com>
2023-08-01 19:49:00 +0000

    Added house object to PropertiesList.jsx & passed it as a prop to PropertyCard component

--- src/components/PropertiesList.jsx
+++ src/components/PropertiesList.jsx
@@ -1,14 +1,19 @@
 import PropertyCard from "./PropertyCard";
+import NavBar from "./NavBar";
 
 function PropertiesList() {
+  const house = {
+    id: 1,
+    house_type: "One Bedroom",
+    rent: 10000,
+    city: "Nairobi",
+    location: "Buruburu",
+  };
   return (
     <>
-      {/* <p>Oh no😢! It seems there are no properties available at the moment.</p> */}
-      <PropertyCard />
-      <PropertyCard />
-      <PropertyCard />
-      <PropertyCard />
-      <PropertyCard />
+      <NavBar />
+      <PropertyCard house={house} />
+      <p>Oh no😢! It seems there are no properties available at the moment.</p>
     </>
   );
 }
