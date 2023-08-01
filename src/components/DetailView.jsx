1a510122294361584ec95fa1f23e19622c774ddb
Brian Waweru <91993455+dexterbrian@users.noreply.github.com>
2023-08-01 20:12:10 +0000

    Displaying house details in the DetailView component

--- src/components/DetailView.jsx
+++ src/components/DetailView.jsx
@@ -1,7 +1,24 @@
-function DetailView() {
+import houseImage from "/house-image.jpg";
+
+function DetailView({ house }) {
   return (
     <>
-      <p>Detail view</p>
+      <h2>
+        {house.house_type} at KES {house.rent} in {house.location}, {house.city}
+      </h2>
+      <img
+        src={houseImage}
+        height="300"
+        onClick={() => {
+          history.push(`/houses/${house.id}`);
+        }}
+      />
+      <p>
+        This {house.house_type} house is located at {house.building} in{" "}
+        {house.location}, {house.city}. To schedule a physical visit, please
+        contact {house.owner} at {house.phone}.
+      </p>
+      <a href={house.coordinates}>View the location on Google Maps</a>
     </>
   );
 }
