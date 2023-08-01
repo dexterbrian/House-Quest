e46ea67e20a17b6e283b42adb529b082d84e44c9
Brian Waweru <91993455+dexterbrian@users.noreply.github.com>
2023-08-01 20:11:36 +0000

    Moved house object to App.jsx and passed it as props to PropertiesList & DetailView

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ import "./App.css";
 import DetailView from "./components/DetailView";
 
 function App() {
+  const house = {
+    id: 1,
+    house_type: "One Bedroom",
+    rent: 10000,
+    city: "Nairobi",
+    location: "Buruburu",
+    building: "Jimlizer Apartment",
+    floor: 2,
+    coordinates: "https://map.google.com/",
+    other_info: "24 hour security, water and parking available",
+    phone: "+254711223344",
+    owner: "Brian",
+  };
   return (
     <>
       <Switch>
@@ -12,10 +25,10 @@ function App() {
           <Home />
         </Route>
         <Route exact path="/houses">
-          <PropertiesList />
+          <PropertiesList house={house} />
         </Route>
         <Route path={`/houses/:propertyId`}>
-          <DetailView />
+          <DetailView house={house} />
         </Route>
       </Switch>
     </>
